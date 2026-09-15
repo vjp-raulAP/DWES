02b85687dc4ede9fb841cdbb2af10b17f88ba73e
vjp-raulAP <ralbalatp01@informatica.iesvalledeljerteplasencia.es>
2026-09-16 00:50:43 +0200

    ejercicio 1 C#

---
+++ Tema1/Ejercicio1/NovaWarehouse/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Package weight (kg): ");   //  Muestra por pantalla 
+string? input = Console.ReadLine();           // "string" -- texto , "?"-- indic que puede ser nulo , "input" -- nombre de la variable , Console.ReadLine()-- Lee teclado
+double weightKg = double.Parse(input!);       //convierto en numero el imput
+
+
+const double baseCost = 3.50;                 //constante  llamo baseCost
+const double costPerKg = 0.80;                //cosntante  llamo costPerKg
+double totalCost = baseCost + (weightKg * costPerKg);
+Console.WriteLine($"Shipping cost:{totalCost:C2}");  //$".." permite meter una variable dentro de texto.  ":C" formatea el número como moneda con 2 decimales
+
+//1. Añade una variable bool isExpress que sume un recargo fi jo de 5€ si es true.
+
+bool isExpress = true;
+
+if (isExpress)
+{
+    totalCost += 5;  //preguntar si se puede hacer
+}
+Console.WriteLine($"Shipping cost whith isExpress: {totalCost:C2}");
